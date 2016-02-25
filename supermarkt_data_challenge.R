4001cc0a9d78623cf812563976ebfa6477b4f1fb
djpappano <djpappano@gmail.com>
2016-02-25 11:48:04 -0500

    Thursday morning commit

--- supermarkt_data_challenge.R
+++ supermarkt_data_challenge.R
@@ -3,19 +3,16 @@ library(readr)
 library(dplyr)
 library(tidyr)
 library(lubridate)
+library(ggplot2)
 
 ##read in raw data from .csv
 dir()
 raw_data<-read_csv("supermarket_data_aggr.csv") %>%
   filter(complete.cases(.))
 
-sort(unique(raw_data$unique_products_purchased))
-sort(unique(raw_data$shops_used))
-sort(unique(raw_data$products_purchased))
-
-
-
-##replicate the three original tables
+##going to try to replicate original tables
+##i'm making the assumign that "shops_used" is a unique identify of one of five shops
+##this is based on other columns in the raw data and appearance that other parameters may correspond to 1-5
 supermarket_distances<-raw_data %>%
   select(customer_id,shops_used,distance_shop_1:distance_shop_5) %>%
   distinct(.) %>%
@@ -25,11 +22,32 @@ supermarket_distances<-raw_data %>%
   mutate(customer_id=as.character(customer_id)) %>%
   mutate(shops_used=as.character(shops_used))
 
+##distance of average consumer to each shop
+distance_fig<-supermarket_distances %>%
+  mutate(shops_used=as.character(shops_used)) %>%
+  select(shops_used,distance) %>%
+  ggplot() +
+  geom_histogram(aes(x=distance,fill=shops_used),colour='black') +
+  geom_vline(xintercept=mean(supermarket_distances$distance)) +
+  theme_grey(base_size=18) +
+  facet_wrap(~shops_used)
+
+##violin plot of customer distance to each shop  
+distance_fig
+
+summary_distances<-supermarket_distances %>%
+  mutate(shops_used=as.character(shops_used)) %>%
+  group_by(shops_used) %>%
+  summarize(count=n(),
+            mean_dist=mean(distance),
+            sd_dist=sd(distance))
+View(summary_distances)
 
 
-supermarket_prices<-raw_data %>%
-  select(unique_products_purchased_shop_1:unique_products_purchased_shop_5,avg_price_shop_1:avg_price_shop_5) %>%
-  distinct(.)
 
-#supermarket_purchases<-raw_data %>%
-  
+
+
+
+
+
+
